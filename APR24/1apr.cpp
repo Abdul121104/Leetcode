1d530956cff62625e8dce1a26b224a21843fa7e6
Abdul121104 <119029386+Abdul121104@users.noreply.github.com>
2024-04-01 15:21:53 +0530

    Create 1apr.cpp

---
+++ APR24/1apr.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int length=0;
+        bool found=false;
+        for (int i=s.size()-1;i>=0;i--) {
+            if (s[i]!= ' ') {
+                found = true;
+                length++;
+            } 
+            else if(found)
+                break;
+        }
+        return length;
+    }
+};
