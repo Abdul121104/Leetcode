b96558d3c044d424e9abcd4fdfa9c060cc4af939
Abdul Rahman <119029386+Abdul121104@users.noreply.github.com>
2025-01-28 20:37:34 +0530

    Create 28.cpp

---
+++ Jan25/28.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int findFish(vector<vector<int>>& grid, int i, int j, int m, int n) {
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == 0) {
+            return 0;
+        }
+        int fish = grid[i][j];
+        grid[i][j] = 0;
+        fish += findFish(grid, i - 1, j, m, n);
+        fish += findFish(grid, i, j + 1, m, n);
+        fish += findFish(grid, i + 1, j, m, n);
+        fish += findFish(grid, i, j - 1, m, n);
+        return fish;
+    }
+
+    int findMaxFish(vector<vector<int>>& grid) {
+        int maxFish = 0;
+        int m = grid.size();
+        int n = grid[0].size();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] > 0) {
+                    maxFish = max(maxFish, findFish(grid, i, j, m, n));
+                }
+            }
+        }
+        return maxFish;
+    }
+};
