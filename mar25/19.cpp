c3c35cb8b30f5176d3b19a3c0c2ecbe586ffeed1
Abdul Rahman <119029386+Abdul121104@users.noreply.github.com>
2025-03-19 16:31:12 +0530

    Create 19.cpp

---
+++ mar25/19.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int ans=0;
+        for(int i=0;i<nums.size()-2;i++){
+            if(nums[i]==0){
+                nums[i]=1-nums[i];
+                nums[i+1]=1-nums[i+1];
+                nums[i+2]=1-nums[i+2];
+                ans++;
+            }
+        }
+        for(int x:nums) if(x==0) return -1;
+        return ans;
+    }
+};
